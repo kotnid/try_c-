56b86fef00ff3ea25ed543c500e1067078eb99ee
kotnid <tkt0506tkt@gmail.com>
2021-08-05 22:44:05 +0800

    try_c+ : first.cpp : calculator func
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- first.cpp
+++ first.cpp
@@ -1,16 +1,38 @@
-#include <iostream>
+# include <iostream>
 using namespace std;
 
-/* test code */
-int main(){
-    string ans;
-    int mynum = 87 ;
-    cout << mynum << endl;
-    cout << "Hihi! \n";
-    cout << "dwd \n";
-    cout << "what is your name :";
-    cin >> ans;
-    cout << "so your name is " << ans << endl; 
+int main() {
+    char op;
+    float num1, num2;
 
+    cout << "Enter operator: +, -, *, /: ";
+    cin >> op;
 
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+    }
+
+    return 0;
 }
