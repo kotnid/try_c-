0029757326a9f13b598c7ed1dc25a0ef0d7d37a5
kotnid <tkt0506tkt@gmail.com>
2021-08-05 21:01:50 +0800

    try_c+ : first.cpp : ask name func
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- first.cpp
+++ first.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+/* test code */
 int main(){
-    cout << "Hihi!";
+    string ans;
+    int mynum = 87 ;
+    cout << mynum << endl;
+    cout << "Hihi! \n";
+    cout << "dwd \n";
+    cout << "what is your name :";
+    cin >> ans;
+    cout << "so your name is " << ans << endl; 
+
+
 }
